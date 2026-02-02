ebc5cc80aa76127b5732ee67645fd26de0b73236
VicoKev <kevinpatinvoh@gmail.com>
2026-02-02 22:22:50 +0100

    initial commit

---
+++ src/main/java/com/ifri/bookmyhall/BookmyhallApplication.java
@@ -0,0 +1,13 @@
+package com.ifri.bookmyhall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyhallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyhallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifri/bookmyhall/BookmyhallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifri.bookmyhall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyhallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
