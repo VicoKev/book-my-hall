90683903f50cdf89de7ca9c28d2c92bf04cd4e81
VicoKev <kevinpatinvoh@gmail.com>
2026-02-03 23:00:55 +0100

    feat: add @Repository annotation to ReservationRepository and UtilisateurRepository

--- src/main/java/com/ifri/bookmyhall/repositories/ReservationRepository.java
+++ src/main/java/com/ifri/bookmyhall/repositories/ReservationRepository.java
@@ -1,9 +1,11 @@
 package com.ifri.bookmyhall.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ifri.bookmyhall.models.Reservation;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
 }
--- src/main/java/com/ifri/bookmyhall/repositories/UtilisateurRepository.java
+++ src/main/java/com/ifri/bookmyhall/repositories/UtilisateurRepository.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.ifri.bookmyhall.models.Role;
 import com.ifri.bookmyhall.models.Utilisateur;
 
+@Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 
     /**
